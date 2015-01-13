dd5a2134460397a98a820f302e4d994a27fa46fc
shaana <shaana@student.ethz.ch>
2015-01-13 22:36:07 +0100

    updated config

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ config["default"] = {
   ["prefix_answers"] = "<DCSpam>: ",
   --if you put this too low you risk getting disconnected
   ["spam_frequency"] = 30, --in seconds
-  ["spam_msg"] = "Deposit Coin (1/7 Mythic) is currently recruiting dps for mythic progression. We raid 2 times per week 19:30 - 23:00. We consist of previous top players on the server who now want to raid more casually. /w for more information.",
+  ["spam_msg"] = "{r1} Deposit Coin (1/7 Mythic) is recruiting dps and exceptional players for progress. We raid 2 times per week 19:30 - 23:00. We consist of previous top players on the server who now want to raid more casually. /w for more information.",
   
   ["commands"] = {
 --    ["!info"] = "Whisper '!info {hm, brf}' for more infos about recruting. Example: '!info mage'.",
